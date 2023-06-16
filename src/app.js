46df75f641c930c8899886577b038c30fab2485d
silverag2002 <silverag2002@gmail.com>
2023-06-16 18:36:27 +0530

    Added packages

---
+++ src/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const compression = require("compression");
+const cors = require("cors");
+const passport = require("passport");
+const httpStatus = require("http-status");
+
+const app = express();
+
+// set security HTTP headers
+app.use(helmet());
+
+// sanitize request data
+app.use(mongoSanitize());
+
+// gzip compression
+app.use(compression());
+
+// enable cors
+app.use(cors());
+app.options("*", cors());
+
+// jwt authentication
+app.use(passport.initialize());
+// passport.use('jwt', jwtStrategy);
