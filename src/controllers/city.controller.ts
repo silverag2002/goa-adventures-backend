54aaf659b2e91bd4983a51721a150d4a217920c5
silverag2002 <silverag2002@gmail.com>
2023-06-20 05:58:19 +0530

    Added controller for city

---
+++ src/controllers/city.controller.ts
@@ -0,0 +1,140 @@
+import { Request, Response } from "express";
+import db from "../models";
+
+const State = db.states;
+
+// Create and Save a new blog
+export const create = async (req: Request, res: Response): Promise<void> => {
+  try {
+    // Validate request
+    if (!req.body.content) {
+      res.status(400).send({ message: "Content can not be empty!" });
+      return;
+    }
+
+    // Save blog in the database
+    const data = await State.create(req.body);
+    res.send(data);
+  } catch (err: any) {
+    res.status(500).send({
+      message: err.message || "Some error occurred while creating the product.",
+    });
+  }
+};
+
+export const findAll = async (req: Request, res: Response): Promise<void> => {
+  console.log("hitted");
+  try {
+    const content = req.query.content;
+    const condition = content ? { state_name: content } : null;
+
+    const data = await State.findAll({ where: condition });
+    res.send(data);
+  } catch (err: any) {
+    console.log(err);
+    res.status(500).send({
+      message: err.message || "Some error occurred while retrieving blogs.",
+    });
+  }
+};
+
+export const findByState = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const state = req.params.state;
+
+    const data = await State.findOne({
+      where: { state_name: state },
+      attributes: ["states"],
+    });
+    if (!data) {
+      res.status(404).send({ message: "Not found blog with id " });
+    } else {
+      res.send(data);
+    }
+  } catch (err) {
+    res.status(500).send({ message: "Error retrieving blog with id=" });
+  }
+};
+
+export const update = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const id = req.params.id;
+
+    const [rowsUpdated] = await State.update(req.body, {
+      where: { id: id },
+    });
+
+    if (rowsUpdated === 0) {
+      res.status(404).send({
+        message: `Cannot update State with id=${id}. Maybe State was not found!`,
+      });
+    } else {
+      res.send({ message: "State was updated successfully." });
+    }
+  } catch (err) {
+    res.status(500).send({
+      message: "Error updating State with id=",
+    });
+  }
+};
+
+export const deleteState = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const id = req.params.id;
+
+    const rowsDeleted = await State.destroy({
+      where: { id: id },
+    });
+
+    if (rowsDeleted === 0) {
+      res.status(404).send({
+        message: `Cannot delete State with id=${id}. Maybe State was not found!`,
+      });
+    } else {
+      res.send({
+        message: "State was deleted successfully!",
+      });
+    }
+  } catch (err) {
+    res.status(500).send({
+      message: "Could not delete Tutorial with ",
+    });
+  }
+};
+
+export const deleteAll = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const rowsDeleted = await State.destroy({
+      where: {},
+      truncate: false,
+    });
+
+    res.send({
+      message: `${rowsDeleted} States were deleted successfully!`,
+    });
+  } catch (err: any) {
+    res.status(500).send({
+      message: err.message || "Some error occurred while removing all blogs.",
+    });
+  }
+};
+
+export const findAllPublished = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const data = await State.findAll({ where: { published: true } });
+    res.send(data);
+  } catch (err: any) {
+    res.status(500).send({
+      message: err.message || "Some error occurred while retrieving blogs.",
+    });
+  }
+};
