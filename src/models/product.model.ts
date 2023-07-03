7fcb6882747521fc6debb085326f966370165deb
silverag2002 <silverag2002@gmail.com>
2023-07-03 18:50:54 +0530

    Fixed the product model

--- src/models/product.model.ts
+++ src/models/product.model.ts
@@ -2,33 +2,35 @@ import { Sequelize, DataTypes, Optional, Model } from "sequelize";
 
 interface ProductAttributes {
   // id: number;
-  title: string;
-  // gallery: string;
-  // featured_image: string;
-  allow_cancel: string;
-  video: string;
-  overview: string;
-  duration: string;
-  creator: string;
-  state: string;
-  category: string;
-  category_type: string;
-  city: string;
-  // country: string;
-  min_people: number;
-  booking_period: number;
-  max_people: number;
-  price: number;
-  deposit: string;
-  deposit_value: number;
-  discount_percent: number;
-  highlight: string;
-  activity_inclusion: any[];
-  activity_exclusion: any[];
+  title: string; //done
+  gallery: string[]; //done
+  featured_image: string; //done
+  allow_cancel: Boolean; //done
+  allow_deposit: Boolean; //done
+  video: string; //done
+  overview: string; //done;
+  duration: string; //done
+  creator: string; //done
+  state: string; //done
+  category: string; //done
+  category_type: string; //done
+  city: string; //done
+  country: string; //done
+  min_people: string; //done
+  booking_period: string; //done
+  max_people: string; //done
+  price: string; //done
+
+  deposit_value: string; //done
+  discount_percent: string; //done
+  highlight: string[]; //done
+  activity_inclusion: string[]; //done
+  activity_exclusion: string[]; //done
+
   start_time: String;
   createdAt: Date;
   updatedAt: Date;
-  // last_update_by: string;
+  last_update_by: string;
 }
 
 module.exports = (sequelize: Sequelize, DataTypes: any) => {
@@ -40,27 +42,29 @@ module.exports = (sequelize: Sequelize, DataTypes: any) => {
     public duration!: string;
     public creator!: string;
     public state!: string;
+    public country!: string;
     public category!: string;
     public category_type!: string;
-    // public gallery!: string;
-    // public featured_image!: string;
+    public gallery!: string[];
+    public featured_image!: string;
     public city!: string;
-    public min_people!: number;
-    public booking_period!: number;
-    public max_people!: number;
-    public price!: number;
-    public deposit_value!: number;
-    public discount_percent!: number;
-    public highlight!: string;
-    public activity_inclusion!: any[];
-    public activity_exclusion!: any[];
+    public min_people!: string;
+    public booking_period!: string;
+    public max_people!: string;
+    public price!: string;
+    public deposit_value!: string;
+
+    public discount_percent!: string;
+    public highlight!: string[];
+    public activity_inclusion!: string[];
+    public activity_exclusion!: string[];
     public start_time!: string;
     public createdAt!: Date;
     public updatedAt!: Date;
-    public deposit!: string;
-    public allow_cancel!: string;
-    // public last_update_by!: string;
-    // public country!: string;
+
+    public allow_cancel!: Boolean;
+    public allow_deposit!: Boolean;
+    public last_update_by!: string;
 
     static associate(models: any) {
       // Define associations with other models
@@ -72,15 +76,18 @@ module.exports = (sequelize: Sequelize, DataTypes: any) => {
       title: {
         type: DataTypes.STRING,
       },
+      country: {
+        type: DataTypes.STRING,
+      },
       video: {
         type: DataTypes.TEXT("long"),
       },
-      // featured_image: {
-      //   type: DataTypes.TEXT("long"),
-      // },
-      // gallery: {
-      //   type: DataTypes.TEXT("long"),
-      // },
+      featured_image: {
+        type: DataTypes.TEXT("long"),
+      },
+      gallery: {
+        type: DataTypes.JSON,
+      },
       overview: {
         type: DataTypes.TEXT("long"),
       },
@@ -91,14 +98,15 @@ module.exports = (sequelize: Sequelize, DataTypes: any) => {
         type: DataTypes.STRING,
       },
       allow_cancel: {
-        type: DataTypes.STRING,
+        type: DataTypes.BOOLEAN,
+      },
+      allow_deposit: {
+        type: DataTypes.BOOLEAN,
       },
       state: {
         type: DataTypes.STRING,
       },
-      // country: {
-      //   type: DataTypes.STRING,
-      // },
+
       category: {
         type: DataTypes.STRING,
       },
@@ -108,29 +116,30 @@ module.exports = (sequelize: Sequelize, DataTypes: any) => {
       city: {
         type: DataTypes.STRING,
       },
-      // last_update_by: {
-      //   type: DataTypes.STRING,
-      // },
+      last_update_by: {
+        type: DataTypes.STRING,
+      },
       min_people: {
-        type: DataTypes.INTEGER,
+        type: DataTypes.STRING,
       },
       booking_period: {
-        type: DataTypes.INTEGER,
+        type: DataTypes.STRING,
       },
       max_people: {
-        type: DataTypes.INTEGER,
-      },
-      price: {
-        type: DataTypes.DECIMAL,
+        type: DataTypes.STRING,
       },
       deposit_value: {
-        type: DataTypes.DECIMAL,
+        type: DataTypes.STRING,
       },
+      price: {
+        type: DataTypes.STRING,
+      },
+
       discount_percent: {
-        type: DataTypes.DECIMAL,
+        type: DataTypes.STRING,
       },
       highlight: {
-        type: DataTypes.TEXT("long"),
+        type: DataTypes.JSON,
       },
       activity_inclusion: {
         type: DataTypes.JSON,
@@ -141,9 +150,7 @@ module.exports = (sequelize: Sequelize, DataTypes: any) => {
       start_time: {
         type: DataTypes.STRING,
       },
-      deposit: {
-        type: DataTypes.STRING,
-      },
+
       createdAt: {
         field: "created_at",
         type: DataTypes.DATE,
